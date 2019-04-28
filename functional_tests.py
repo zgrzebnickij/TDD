cbbb6e1b156f8bf21a0e802faab2ea0d84e4b8a2
Jakub Zgrzebnicki <jakub.zgrzeb@gmail.com>
2019-04-28 21:18:43 +0200

    used unitests for testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get("http://localhost:8000")
+class NewVistiorTests(unittest.TestCase):
 
-assert "Django" in browser.title
+  def setUp(self):
+    self.browser = webdriver.Chrome()
+    self.browser.implicitly_wait(2)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrive_it_later(self):
+    self.browser.get("http://localhost:8000")
+
+    # Page title and header mention to-do list
+    assert "To-Do" in self.browser.title
+
+    # Invitation for entering to-do item
+
+    # Typing "Buy spoon" into text box
+
+    # Updating page after hiting enter
+    # Visible new item in list
+
+    # Stil visible text box
+    # entering new todo
+
+    # second update on page and to wisible items
+
+    # Does the page remember items? Uniqe ulrs
+
+if __name__ == "__main__":
+  unittest.main(warnings='ignore')
