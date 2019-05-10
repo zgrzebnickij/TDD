d6f24f340deeb9981c443bde4ee279334ee353b3
Jakub Zgrzebnicki <jakub.zgrzeb@gmail.com>
2019-05-10 18:22:27 +0200

    added lists in models

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+class List(models.Model):
+  pass
 # Create your models here.
 class Item(models.Model):
   text = models.TextField(default="")
+  list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
+
